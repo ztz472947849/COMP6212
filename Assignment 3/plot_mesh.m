6e2ded3320db0b1b8fa6fbbd7be2f29feafbf5be
ztz472947849 <ztz_1@qq.com>
2017-05-09 20:52:03 +0100

    Update plot_mesh.m

--- Assignment 3/plot_mesh.m
+++ Assignment 3/plot_mesh.m
@@ -1,12 +1,12 @@
 function [] = plot_mesh(X, Y, Z)
     figure;
     F = TriScatteredInterp(X, Y, Z);
+    %F = scatteredInterpolant(X,Y,Z);
     rx = (max(X) - min(X)) / 20;
     ry = (max(Y) - min(Y)) / 20;
     [qx,qy] = meshgrid(min(X):rx:max(X), min(Y):ry:max(Y));
     qz = F(qx,qy);
     mesh(qx,qy,qz);
     hold on;
-    scatter3(X, Y, Z);
+    scatter3(X, Y, Z,10,'filled');
 end
-
