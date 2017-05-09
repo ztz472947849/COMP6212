9390acbb179bfcba7f09d346e1594e01369f90d7
ztz472947849 <ztz_1@qq.com>
2017-05-09 17:16:03 +0100

    Update p1.m

--- Assignment 3/p1.m
+++ Assignment 3/p1.m
@@ -14,35 +14,41 @@ prices2925 = c2925(:, 3);
 N = size(prices2925, 1);
 volatility = std(tick2ret(prices2925, [], 'Continuous')) / sqrt(N / 252);
 [call, put] = blsprice(prices2925, 2925, 0.06, t, volatility);
-delta2925 = blsdelta(call + 1, 2925, 0.06, t + 1, volatility);
+%delta2925 = blsdelta(call + 1, 2925, 0.06, t + 1, volatility);
+delta2925 = blsdelta(prices2925, 2925, 0.06, t+0.0001, volatility,0);
 call2925 = call / 2925;
 
 prices3025 = c3025(:, 3);
 N = size(prices3025, 1);
 volatility = std(tick2ret(prices3025, [], 'Continuous')) / sqrt(N / 252);
 [call, put] = blsprice(prices3025, 3025, 0.06, t, volatility);
-delta3025 = blsdelta(call + 1, 3025, 0.06, t + 1, volatility);
+%delta3025 = blsdelta(call + 1, 3025, 0.06, t + 1, volatility);
+delta3025 = blsdelta(prices3025, 3025, 0.06, t+0.0001, volatility,0);
 call3025 = call / 3025;
 
 prices3125 = c3125(:, 3);
 N = size(prices3125, 1);
 volatility = std(tick2ret(prices3125, [], 'Continuous')) / sqrt(N / 252);
 [call, put] = blsprice(prices3125, 3125, 0.06, t, volatility);
-delta3125 = blsdelta(call + 1, 3125, 0.06, t + 1, volatility);
+%delta3125 = blsdelta(call + 1, 3125, 0.06, t + 1, volatility);
+delta3125 = blsdelta(prices3125, 3125, 0.06, t+0.0001, volatility,0);
 call3125 = call / 3125;
 
 prices3225 = c3225(:, 3);
 N = size(prices3225, 1);
 volatility = std(tick2ret(prices3225, [], 'Continuous')) / sqrt(N / 252);
 [call, put] = blsprice(prices3225, 3225, 0.06, t, volatility);
-delta3225 = blsdelta(call + 1, 3225, 0.06, t + 1, volatility);
+%delta3225 = blsdelta(call + 1, 3225, 0.06, t + 1, volatility);
+delta3225 = blsdelta(prices3225, 3225, 0.06, t+0.0001, volatility,0);
 call3225 = call / 3225;
 
 prices3325 = c3325(:, 3);
 N = size(prices3325, 1);
 volatility = std(tick2ret(prices3325, [], 'Continuous')) / sqrt(N / 252);
 [call, put] = blsprice(prices3325, 3325, 0.06, t, volatility);
-delta3325 = blsdelta(call + 1, 3325, 0.06, t + 1, volatility);
+%delta3325 = blsdelta(call, 3325, 0.06, t, volatility);
+delta3325 = blsdelta(prices3325, 3325, 0.06, t+0.0001, volatility,0);
+
 call3325 = call / 3325;
 
 % Black-Scholes predicted call option prices normalised by strike price
@@ -55,3 +61,8 @@ plot_mesh(actual_prices, times, bls_prices);
 xlabel('S/X');
 ylabel('T - t');
 zlabel('C/X');
+
+plot_mesh(actual_prices, times, bls_deltas);
+xlabel('S/X');
+ylabel('T - t');
+zlabel('delta');
